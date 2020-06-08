c6947ab32cf48f33a8f35db81727fabdaa496125
little-Dave <17536424+little-Dave@users.noreply.github.com>
2020-06-07 22:38:42 -0600

    add response
    use response methods to supply content

--- first-app.js
+++ first-app.js
@@ -2,6 +2,13 @@ const http = require('http');
 
 const server = http.createServer((req, resp) => {
   console.log(req);
+  // process.exit();
+  resp.setHeader('Content-Type', 'text/html');
+  resp.write('<html>');
+  resp.write('<head><title>Node</title></head>')
+  resp.write('<body>a response?! *gasp*</body>')
+  resp.write('</html>');
+  resp.end();
 });
 
 server.listen(3000);
